ef6b2d2bd20cd00cfd010827d7d1d79a0884cd7a
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-10-17 18:38:53 +0530

    Update dvwa.sh

--- dvwa.sh
+++ dvwa.sh
@@ -1,13 +1,13 @@
 
 #!/bin/bash
-for i in 1 2 3 4 5
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
-   echo "ENSURE YOU ARE SUDO if not pleaee press cntrl+C and do sudo su within 30 seconds $i times"
+   echo "ENSURE YOU ARE SUDO if not pleaee press cntrl+C and do sudo su within 20-$i seconds"
    
-   sleep 2
+   sleep 1
 done
 
-sleep 10
+sleep 2
 
 
 cd $HOME
