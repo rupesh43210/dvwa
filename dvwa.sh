afdcc34e85c1afa3637602ca269c3ec5c77f844e
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-10-17 17:08:13 +0530

    Update dvwa.sh

--- dvwa.sh
+++ dvwa.sh
@@ -2,28 +2,16 @@
 
 apt update
 apt install -y apache2 mariadb-server mariadb-client php php-mysqli php-gd libapache2-mod-php
-
 apt install -y git
-
 cd /var/www/html/
-
 rm index.html
-
-
+mkdir /dvwa
+cd /dvwa
 git clone https://github.com/digininja/DVWA.git
-
 chown -R 777 DVWA
-
 mv ./DVWA/* ./
-
 cp /var/www/html/config/config.inc.php.dist /var/www/html/config/config.inc.php
-
-
 clear
-
-
 echo "////////////////////Its time to setup database///////////////////////////"
-
-chmod +x db.sh
-
-./db.sh
+chmod +x /dvwa/db.sh
+/dvwa/db.sh
